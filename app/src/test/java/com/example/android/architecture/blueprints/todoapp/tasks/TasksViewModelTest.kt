df288ff1863d02d238d5c1a789b9d4207df0cebf
jisang <jisangman89@gmail.com>
2022-12-17 23:10:04 +0900

    Update : Task: Writing Assertions for LiveData

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,22 +1,52 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
+import org.hamcrest.CoreMatchers.nullValue
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.core.IsNot.not
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class TasksViewModelTest {
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
     @Test
     fun addNewTask_setsNewTaskEvent() {
 
         // Given a fresh TasksViewModel
         val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
 
+//        val observer = Observer<Event<Unit>> {}
+//        try {
+//            // Observe the LiveData forever
+//            tasksViewModel.newTaskEvent.observeForever(observer)
+//
+//            // When adding a new task
+//            tasksViewModel.addNewTask()
+//
+//            // Then the new task event is triggered
+//            val value = tasksViewModel.newTaskEvent.value
+//            assertThat(value?.getContentIfNotHandled(), (not(nullValue())))
+//
+//        } finally {
+//            // Whatever happens, don't forget to remove the observer!
+//            tasksViewModel.newTaskEvent.removeObserver(observer)
+//        }
+
         // When adding a new task
         tasksViewModel.addNewTask()
 
         // Then the new task event is triggered
+        val value = tasksViewModel.newTaskEvent.getOrAwaitValue()
+
+        assertThat(value.getContentIfNotHandled(), not(nullValue()))
 
     }
 }
